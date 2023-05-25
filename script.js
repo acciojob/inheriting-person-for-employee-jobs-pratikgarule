94957781b838e5863956374f7060ac62780ab16b
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-05-25 11:06:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// complete this js code
-/*function Person(name, age){
-	greet(){
-		console.log(`Hello, my name is ${this.name} I am ${this.age} years old.`);
-	}
-}
-
-function Employee(name, age, jobTitle){
-  Person.apply(this, [name, age]);
-  this.jobTitle = jobTitle;
-	jobGreet(){
-		console.log(`Hello, my name is ${this.name} I am ${this.age} years old, and my job title is ${this.jobTitle}`);
-	}
-}
-Employee.prototype = new Person();*/
 
 function Person(name, age) {
   this.name = name;
