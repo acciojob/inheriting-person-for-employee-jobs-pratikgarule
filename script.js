6ef73f5c4beeb5641085b141c5fbc8e445374023
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-05-25 11:02:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 // complete this js code
-// complete this js code
-function Person(name, age) {
-	this.name=name;
-	this.age=age;
+/*function Person(name, age){
+	greet(){
+		console.log(`Hello, my name is ${this.name} I am ${this.age} years old.`);
+	}
 }
-Person.prototype.greet=function(){
-	return "Hello, my name is " + this.name +", I am "+this.age+" years old.";
+
+function Employee(name, age, jobTitle){
+  Person.apply(this, [name, age]);
+  this.jobTitle = jobTitle;
+	jobGreet(){
+		console.log(`Hello, my name is ${this.name} I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+	}
+}
+Employee.prototype = new Person();*/
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+Person.prototype.greet = function() { //method using prototype
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
-// var p=new Person('areeb',23);
-// console.log(p);
-// console.log(Object.getPrototypeOf(p));
+
 
 function Employee(name, age, jobTitle) {
-	Person.call(this,name,age);
-	this.jobTitle=jobTitle;
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
 
-Employee.prototype=Object.create(Person.prototype);
-Employee.prototype.jobGreet=function(){
-	return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+Employee.prototype = Object.create(Person.prototype);// prototype of person is inherited into employee
+Employee.prototype.constructor = Employee; //get constructor of employee
+
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
 
 // Do not change code below this line
